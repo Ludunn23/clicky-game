a17a4e8b6dbd452a398d0caf8a7399a05166f178
ludunn23 <ludunn23@gmail.com>
2018-07-02 22:13:14 -0600

    Final Draft

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import arya from './images/arya.jpg';
+import baylesh from './images/baylesh.jpg';
+import brienne from './images/brienne.jpg';
+import cersi from './images/cersi.jpg';
+import jonsnow from './images/jonsnow.jpg';
+import khaldrogo from './images/khaldrogo.jpg';
+import khaleesi from './images/khaleesi.png';
+import ramsay from './images/ramsay.jpg';
+import sansa from './images/sansa.jpg';
+import tormund from './images/tormund.jpg';
+import tyrion from './images/tyrion.jpg';
+import ygritte from './images/ygritte.jpg';
+
+const images = [
+  arya,
+  baylesh,
+  brienne,
+  cersi,
+  jonsnow,
+  khaldrogo,
+  khaleesi,
+  ramsay,
+  sansa,
+  tormund,
+  tyrion,
+  ygritte
+];
+
+export default images;
